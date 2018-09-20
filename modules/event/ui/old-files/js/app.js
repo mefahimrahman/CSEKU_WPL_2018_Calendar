e9526d747bcd31be54b5d0a1cd86262bcd4ad175
mefahimrahman <mefahimrahman@gmail.com>
2018-09-21 02:34:35 +0600

    Month View Edited

--- modules/event/ui/old-files/js/app.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.tabs button').click(function(event) {
-		var selector = $(this).data('open');
-		$('.tab-container .content').removeClass('active');
-		$(selector).addClass('active');
-	});
-});
